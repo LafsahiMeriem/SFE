0f86bf6a8aa33e439757a33857eda06f646aa1e4
MeriemLafsahi670 <mlafsahi63@gmal.com>
2024-03-13 10:58:43 +0000

    commit

--- lib/EncoderPage.dart
+++ lib/EncoderPage.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'database_helper.dart';
 
-class EncodePage extends StatelessWidget {
-  const EncodePage({Key? key});
+class EncodePage extends StatefulWidget {
+  const EncodePage({Key? key}) : super(key: key);
+
+  @override
+  _EncodePageState createState() => _EncodePageState();
+}
+
+class _EncodePageState extends State<EncodePage> {
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,23 +29,135 @@ class EncodePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Encoder'),
       ),
-      body: const Center(
-        child: Text('Encoder Page'),
+      body: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20), // Add some spacing
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Chercher un produit...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                onSubmitted: (value) {
+                  _searchProduct(context, value);
+                },
+              ),
+            ),
+            const SizedBox(height: 20), // Add some spacing
+            const Text('Encoder Page'),
+          ],
+        ),
       ),
       bottomNavigationBar: SafeArea(
         child: BottomAppBar(
           child: Container(
             height: 60, // Set the height of the BottomAppBar
-            child: EncoderBottomBar(),
+            child: EncoderBottomBar(
+              onEncodePressed: () {
+                _searchProductWithBarcode(context);
+              },
+            ),
           ),
         ),
       ),
     );
   }
+
+  void _searchProduct(BuildContext context, String productName) async {
+    final DatabaseHelper dbHelper = DatabaseHelper.instance;
+    final List<Map<String, dynamic>> products = await dbHelper.queryAllRows();
+
+    // Filter products based on the productName
+    List<Map<String, dynamic>> filteredProducts = products.where((product) =>
+        product[DatabaseHelper.columnName].toString().toLowerCase().contains(productName.toLowerCase())
+    ).toList();
+
+    _showProductsDialog(context, filteredProducts);
+  }
+
+  void _searchProductWithBarcode(BuildContext context) async {
+    final DatabaseHelper dbHelper = DatabaseHelper.instance;
+    final List<Map<String, dynamic>> products = await dbHelper.queryAllRows();
+
+    // Filter products with non-empty barcode
+    List<Map<String, dynamic>> filteredProducts = products.where((product) =>
+    product[DatabaseHelper.columnBarcode].toString().isNotEmpty
+    ).toList();
+
+    _showProductsDialog(context, filteredProducts);
+  }
+
+  void _showProductsDialog(BuildContext context, List<Map<String, dynamic>> products) {
+    if (products.isNotEmpty) {
+      // Display information of the products
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Product Information'),
+            content: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: products.map((product) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Product Name: ${product[DatabaseHelper.columnName]}'),
+                      Text('Barcode: ${product[DatabaseHelper.columnBarcode]}'),
+                      Text('Building: ${product[DatabaseHelper.columnBuilding]}'),
+                      Text('Floor: ${product[DatabaseHelper.columnFloor]}'),
+                      Text('Zone: ${product[DatabaseHelper.columnZone]}'),
+                      Text('Reference: ${product[DatabaseHelper.columnReference]}'),
+                      const Divider(), // Add a divider between products
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      // Display a message if no product found
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Product Not Found'),
+            content: const Text('No product found with the specified condition.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 }
 
 class EncoderBottomBar extends StatelessWidget {
-  const EncoderBottomBar({Key? key});
+  final VoidCallback onEncodePressed;
+
+  const EncoderBottomBar({Key? key, required this.onEncodePressed}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -43,9 +175,7 @@ class EncoderBottomBar extends StatelessWidget {
         _buildBottomBarItem(
           icon: Icons.qr_code,
           label: 'Encoder',
-          onPressed: () {
-            // Add logic for encoding action
-          },
+          onPressed: onEncodePressed,
         ),
         _buildBottomBarItem(
           icon: Icons.qr_code_outlined,

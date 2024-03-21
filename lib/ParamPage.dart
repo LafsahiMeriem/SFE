f280d308de76f2972ae6edcaaeffd1792b27862f
MeriemLafsahi670 <mlafsahi63@gmal.com>
2024-03-21 09:49:48 +0000

    commit

--- lib/ParamPage.dart
+++ lib/ParamPage.dart
@@ -42,7 +42,7 @@ class _ParamPageState extends State<ParamPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Liste des bâtiments'),
+        title: Text('Liste des bâtiments '),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -127,62 +127,107 @@ class ZonePage extends StatefulWidget {
 class _ZonePageState extends State<ZonePage> {
   late TextEditingController _zoneController;
   bool _isAddingZone = false;
+  List<String> zones = [];
+
 
   @override
   void initState() {
     super.initState();
     _zoneController = TextEditingController();
+    _loadZones();
+
   }
+  Future<void> _loadZones() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      zones = prefs.getStringList('zones') ?? [];
+    });
+  }
+
+  Future<void> _saveZones() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('zones', zones);
+  }
+
 
   @override
   void dispose() {
     _zoneController.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Zones de ${widget.buildingName}'),
+        title: Text('Liste des zones'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: _isAddingZone
-            ? Column(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              controller: _zoneController,
-              decoration: InputDecoration(
-                hintText: 'Nom de la zone',
+            Expanded(
+              child: ListView.builder(
+                itemCount: zones.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(zones[index]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ZonePage(zones[index])),
+                      );
+                    },
+                  );
+                },
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                String zoneName = _zoneController.text.trim();
-                if (zoneName.isNotEmpty) {
-                  setState(() {
-                    // Ajouter la zone au bâtiment actuel
-                    // Vous pouvez ajouter ici la logique pour sauvegarder la zone dans la base de données
-                    _zoneController.clear();
-                    _isAddingZone = false;
-                  });
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Zone ajoutée avec succès: $zoneName'),
-                    ),
-                  );
-                }
-              },
-              child: Text('Ajouter'),
-            ),
+            _buildAddZoneButton(),
           ],
-        )
-            : SizedBox(), // Afficher un SizedBox pour garder l'espace vide lorsque le formulaire n'est pas affiché
+        ),
       ),
+    );
+  }
 
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+  Widget _buildAddZoneButton() {
+    return _isAddingZone
+        ? Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          controller:_zoneController,
+          decoration: InputDecoration(
+            hintText: 'Nom du zone',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            String zoneName = _zoneController.text.trim();
+            if (zoneName.isNotEmpty) {
+              setState(() {
+                zones.add(zoneName);
+                _saveZones(); // Sauvegarde des bâtiments
+                _zoneController.clear();
+                _isAddingZone  = false;
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Zone ajouté avec succès: $zoneName'),
+                ),
+              );
+            }
+          },
+          child: Text('Ajouter'),
+        ),
+      ],
+    )
+        : ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _isAddingZone  = true;
+        });
+      },
+      child: Text('Ajouter un bâtiment'),
     );
   }
 }

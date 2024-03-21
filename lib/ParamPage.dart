542c8d47165c0b52c437af757526a123d66995fe
MeriemLafsahi670 <mlafsahi63@gmal.com>
2024-03-21 10:14:32 +0000

    commit

--- lib/ParamPage.dart
+++ lib/ParamPage.dart
@@ -42,7 +42,7 @@ class _ParamPageState extends State<ParamPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Liste des bâtiments '),
+        title: Text(' Bâtiments '),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -159,7 +159,7 @@ class _ZonePageState extends State<ZonePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Liste des zones'),
+        title: Text('Les zones du ${widget.buildingName}'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -175,9 +175,10 @@ class _ZonePageState extends State<ZonePage> {
                     onTap: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => ZonePage(zones[index])),
+                        MaterialPageRoute(builder: (context) => FloorPage(zones[index])),
                       );
                     },
+
                   );
                 },
               ),
@@ -227,10 +228,215 @@ class _ZonePageState extends State<ZonePage> {
           _isAddingZone  = true;
         });
       },
-      child: Text('Ajouter un bâtiment'),
+      child: Text('Ajouter une zone'),
     );
   }
 }
+class FloorPage extends StatefulWidget {
+  final String zoneName;
+
+  const FloorPage(this.zoneName);
+
+  @override
+  _FloorPageState createState() => _FloorPageState();
+}
+
+class _FloorPageState extends State<FloorPage> {
+  late TextEditingController _floorController;
+  List<String> floors = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _floorController = TextEditingController();
+    _loadFloors();
+  }
+
+  Future<void> _loadFloors() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      floors = prefs.getStringList('${widget.zoneName}_floors') ?? [];
+    });
+  }
+
+  Future<void> _saveFloors() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('${widget.zoneName}_floors', floors);
+  }
+
+  @override
+  void dispose() {
+    _floorController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Les étages de ${widget.zoneName}'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: floors.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(floors[index]),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => OfficePage(floors[index])),
+                      );
+                    },
+
+                  );
+                },
+              ),
+            ),
+            _buildAddFloorButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAddFloorButton() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          controller: _floorController,
+          decoration: InputDecoration(
+            hintText: 'Numéro de l\'étage',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            String floorName = _floorController.text.trim();
+            if (floorName.isNotEmpty) {
+              setState(() {
+                floors.add(floorName);
+                _saveFloors();
+                _floorController.clear();
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Étage ajouté avec succès: $floorName'),
+                ),
+              );
+            }
+          },
+          child: Text('Ajouter un étage'),
+        ),
+      ],
+    );
+  }
+}
+
+class OfficePage extends StatefulWidget {
+  final String floorName;
+
+  const OfficePage(this.floorName);
+
+  @override
+  _OfficePageState createState() => _OfficePageState();
+}
+
+class _OfficePageState extends State<OfficePage> {
+  late TextEditingController _officeController;
+  List<String> offices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _officeController = TextEditingController();
+    _loadOffices();
+  }
+
+  Future<void> _loadOffices() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      offices = prefs.getStringList('${widget.floorName}_offices') ?? [];
+    });
+  }
+
+  Future<void> _saveOffices() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('${widget.floorName}_offices', offices);
+  }
+
+  @override
+  void dispose() {
+    _officeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bureaux de l\'étage ${widget.floorName}'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: offices.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(offices[index]),
+                  );
+                },
+              ),
+            ),
+            _buildAddOfficeButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAddOfficeButton() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          controller: _officeController,
+          decoration: InputDecoration(
+            hintText: 'Nom ou numéro du bureau',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            String officeName = _officeController.text.trim();
+            if (officeName.isNotEmpty) {
+              setState(() {
+                offices.add(officeName);
+                _saveOffices();
+                _officeController.clear();
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Bureau ajouté avec succès: $officeName'),
+                ),
+              );
+            }
+          },
+          child: Text('Ajouter un bureau'),
+        ),
+      ],
+    );
+  }
+}
+
 
 void main() {
   runApp(MaterialApp(
